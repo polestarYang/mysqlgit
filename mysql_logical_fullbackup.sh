2c03fb487cc03bbc5ed8a2dda07738bdac871c8d
polestarYang <46125376+polestarYang@users.noreply.github.com>
2019-07-17 10:13:48 +0800

    Update mysql_logical_fullbackup.sh

--- mysql_logical_fullbackup.sh
+++ mysql_logical_fullbackup.sh
@@ -17,12 +17,12 @@ backup_log=full_backup_dcm.log
 echo "--------------------Full Backup Starting------------------"  >> $backup_dir/$backup_log 
 date >> $backup_dir/$backup_log
 
-/mysqlsoft/mysql/bin/mysqldump -u root -pAirchina_869 --single-transaction --master-data=2 --all-databases -E -R --triggers --set-gtid-purged=o
-ff --socket=/tmp/mysql.sock > $backup_dir/logigal_fullDB_`date '+%m-%d-%Y'`.sql
+/mysqlsoft/mysql/bin/mysqldump -u root -pAirchina_869 --single-transaction --master-data=2 --all-databases \
+-E -R --triggers --set-gtid-purged=off --socket=/tmp/mysql.sock \
+> $backup_dir/logigal_fullDB_`date '+%m-%d-%Y'`.sql
 
 echo "--------------------Full Backup Ended------------------"  >> $backup_dir/$backup_log
 date >> $backup_dir/$backup_log
 
 backup_log=/mysqldata/backup
 find $backup_log -type f -name "logigal_fullDB_*.sql" -mtime +7 -exec rm -rf {} \;
-/
